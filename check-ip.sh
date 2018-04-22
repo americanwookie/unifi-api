7dc22d3a1c7accdb789b3e2814342bb86f13fc46
Scott O'Neil <americanwookie@gmail.com>
2018-04-22 17:49:22 -0500

    Be sure to update the host so that we can run commands there too

--- check-ip.sh
+++ check-ip.sh
@@ -3,3 +3,8 @@ IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}
 if [ ! -z $IP ]; then
   docker exec -it home-assistant-2018-b sh /fix-hosts-u-controller.sh $IP
 fi
+if grep unifi-api /etc/hosts > /dev/null; then
+  sed -i -e "s/^[0-9.]\+ unifi-api/$IP unifi-api/g" /etc/hosts
+else
+  echo "$IP unifi-api" >> /etc/hosts
+fi
