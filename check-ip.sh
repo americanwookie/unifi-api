a79fe3ddc059e4257103d0c24f028b7c345cb0b2
Scott O'Neil <americanwookie@gmail.com>
2019-04-18 14:57:22 +0100

    Updating name of container (this should be more automated)

--- check-ip.sh
+++ check-ip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' unifi-api)
 if [ ! -z $IP ]; then
-  docker exec -it home-assistant-2018-b sh /fix-hosts-u-controller.sh $IP
+  docker exec -it home-assistant-2019-b sh /fix-hosts-u-controller.sh $IP
 fi
 if grep unifi-api /etc/hosts > /dev/null; then
   sed -i -e "s/^[0-9.]\+ unifi-api/$IP unifi-api/g" /etc/hosts
